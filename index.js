d7a0b41d177f903e26ef006de2070c39bf6eb7a6
wayne <waynejwliang@gmail.com>
2016-07-01 12:09:02 +0800

    check auth demo

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var qq = require('./lib/checkQq');
-var wechat = require('./lib/checkWechat');
-var weibo = require('./lib/checkWeibo');
-module.exports = function(){
-    return {
-        qq: qq,
-        wechat: wechat,
-        weibo: weibo
-    };
-};
+module.exports = require('./lib/common');
